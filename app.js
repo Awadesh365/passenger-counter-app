6ea600a7e67dce7f0cfaaaac3234b45bb037ad86
awadesh <36awadesh@gmail.com>
2023-09-18 00:49:05 +0530

    final Commit: Production ready

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
-// document.getElementById("count-el").innerText = 5;
+/*
+    Awadesh Nautiyal
+*/
 
 //-----------PeopleCounter Logic-------------
 
-let saveEl = document.getElementById("save-el");
-let countEl = document.getElementById("count-el");
+// Variables to store references to HTML elements
+let countEl, saveEl, totalEl;
 
+// Function to initialize references to HTML elements
+function essentialFunction() {
+  countEl = document.getElementById("count-el");
+  saveEl = document.getElementById("save-el");
+  totalEl = document.getElementById("total-el");
+}
+
+// Initialize counters for total and current count
 let total = 0,
   count = 0;
 
-let Total = document.getElementById("total-el");
-
+// Function to increment the count
 function increment() {
+  essentialFunction();
+
+  // Increment both current and total counts
   count++;
   total++;
 
-  countEl.innerText = count;
+  // Update the displayed count
+  countEl.textContent = count;
 }
 
+// Function to save the current count
 function save() {
+  essentialFunction();
+
+  // Format the current count as previous entry
   let previousEntries = " " + count + " " + " - ";
 
-  saveEl.innerText += previousEntriese;
+  // Append the previous entry to the list
+  saveEl.textContent += previousEntries;
 
+  // Log the total count to the console
   console.log(total);
 
+  // Reset the current count and update the displayed count
   count = 0;
+  countEl.textContent = 0;
 }
 
+// Function to show the total count
 function showTotal() {
-  document.getElementById("total-el").innerText =
-    "Total People On Platform:-" + total;
+  essentialFunction();
+
+  // Update the displayed total count
+  totalEl.textContent = "Total People On Platform:-" + total;
+
+  // Reset the current count and update the displayed count and entries
   count = 0;
+  countEl.textContent = 0;
+  saveEl.textContent = "Previous entries:";
+}
+
+// Function to reset and start from scratch
+function letStartFromScratch() {
+  // Reset current and total counts, and update displayed elements
+  count = 0;
+  countEl.textContent = 0;
+  saveEl.textContent = "Previous entries:";
+  total = 0;
+  totalEl.textContent = "Total People On Platform:-";
 }
